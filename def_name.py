a51882e109603f5a8b3d0a9b72c2c889a96f5fa7
Savita_N <62276914+Savita01@users.noreply.github.com>
2020-03-17 23:21:11 +0530

    def

---
+++ def_name.py
@@ -0,0 +1,13 @@
+def wel(name):
+    print("Hello ",name +"!")
+wel("Savi")
+
+def num(num1,num2):
+    print(num1+num2)
+num(2,25)
+
+def num(num1,num2):
+    return (num1+num2)
+sum=num(25,85)
+print(sum)
+
